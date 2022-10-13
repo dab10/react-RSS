d348ba4fc9bb4de11e7e7293abb26783f124916e
dab10 <dab10@list.ru>
2022-10-13 23:26:44 +0300

    fix: fix choose class with classnames utility

--- react-typescript/src/components/FormFields/MessageError/MessageError.tsx
+++ react-typescript/src/components/FormFields/MessageError/MessageError.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './MessageError.scss';
+import classNames from 'classnames';
 
 type MessageErrorProps = { messageError: string };
 
-const MessageError = (props: MessageErrorProps) => (
-  <div className={`hidden ${props.messageError ? 'error' : ''}`}>{props.messageError}</div>
-);
+const MessageError = (props: MessageErrorProps) => {
+  const hiddenClass = classNames({
+    hidden: true,
+    error: props.messageError,
+  });
+
+  return <div className={`${hiddenClass}`}>{props.messageError}</div>;
+};
 
 export default MessageError;
