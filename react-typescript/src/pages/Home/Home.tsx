e96dcdb318b0300206c8c0ecba45b98f86dc0576
dab10 <dab10@list.ru>
2022-10-22 02:02:22 +0300

    refactor: refactor other function to function component

--- react-typescript/src/pages/Home/Home.tsx
+++ react-typescript/src/pages/Home/Home.tsx
@@ -32,52 +32,67 @@ type HomeState = {
   errMessage: string | null;
 };
 
-type HomeProps = { [x: string]: string };
+// type HomeProps = { [x: string]: string };
 
 function Home() {
   // base: string;
   // characterByName: string;
 
-    const base = 'https://rickandmortyapi.com/api';
-    const characterByName = `${base}/character/?name=`;
-    const stateInit = {
-      cards: {
-        results: [],
-      },
-      cardModal: {
-        id: 0,
-        image: '',
-        name: '',
-        status: '',
-        species: '',
-        type: '',
-        gender: '',
-        location: {
+  const base = 'https://rickandmortyapi.com/api';
+  const characterByName = `${base}/character/?name=`;
+  const stateInit = {
+    cards: {
+      results: [
+        {
+          id: 0,
+          image: '',
           name: '',
+          status: '',
+          species: '',
+          type: '',
+          gender: '',
+          location: {
+            name: '',
+          },
+          isFavorite: false,
         },
-        isFavorite: false,
+      ],
+    },
+    cardModal: {
+      id: 0,
+      image: '',
+      name: '',
+      status: '',
+      species: '',
+      type: '',
+      gender: '',
+      location: {
+        name: '',
       },
-      searching: '',
-      isLoading: false,
-      isFirstCall: true,
-      isPopup: false,
-      errMessage: '',
-    };
-    const [data, setData] = useState(stateInit);
-    const [url, setUrl] = useState('');
+      isFavorite: false,
+    },
+    searching: '',
+    isLoading: false,
+    isFirstCall: true,
+    isPopup: false,
+    errMessage: '',
+  };
 
- useEffect(() => {
-  const saveSearching = localStorage.getItem('savedStateSearching');
-  if (saveSearching) {
-    setData((prevData) => {
-      return {
-        ...prevData,
-        isFirstCall: JSON.parse(saveSearching) === null ? true : false,
-        searching: JSON.parse(saveSearching),
-      }
-    })
-  }
- }, []);
+  const [data, setData] = useState(stateInit);
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    const saveSearching = localStorage.getItem('savedStateSearching');
+    if (saveSearching) {
+      setData((prevData) => {
+        return {
+          ...prevData,
+          isFirstCall: JSON.parse(saveSearching) === null ? true : false,
+          searching: JSON.parse(saveSearching),
+        };
+      });
+    }
+  }, []);
 
   // async componentDidMount() {
   //   let saveSearching = '';
@@ -93,56 +108,67 @@ function Home() {
   //   this.fetchData(`${this.characterByName}${this.state.searching}`);
   // }
 
-  useEffect(() => { localStorage.setItem('savedStateSearching', JSON.stringify(data.searching)); } );
+  useEffect(() => {
+    localStorage.setItem('savedStateSearching', JSON.stringify(data.searching));
+  });
 
- useEffect(() => {
-  const fetchData = async () => {
-    setData((prevData) => {
-      return {
-        ...prevData,
-        isLoading: true,
-        errMessage: '',
-      }
-    })
+  useEffect(() => {
+    const fetchData = async () => {
+      setData((prevData) => {
+        return {
+          ...prevData,
+          isLoading: true,
+          errMessage: '',
+        };
+      });
 
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      if (!res.ok) {
-        throw Error();
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        if (!res.ok) {
+          throw Error();
+        }
+        setData((prevData) => {
+          return {
+            ...prevData,
+            cards: {
+              results: data.results,
+            },
+          };
+        });
+      } catch {
+        data.isFirstCall
+          ? setData((prevData) => {
+              return {
+                ...prevData,
+                isFirstCall: false,
+                errMessage: '',
+              };
+            })
+          : setData((prevData) => {
+              return {
+                ...prevData,
+                errMessage: 'Could not fetch the data',
+              };
+            });
       }
       setData((prevData) => {
         return {
           ...prevData,
-          cards: {
-            results: data.results,
-          }
-        }
-      })
-    } catch {
-      data.isFirstCall ? setData((prevData) => {
-        return {
-          ...prevData,
-          isFirstCall: false,
-          errMessage: '',
-        }
-      }) : setData((prevData) => {
-        return {
-          ...prevData,
-          errMessage: 'Could not fetch the data',
-        }
-      })
-    }
+          isLoading: false,
+        };
+      });
+    };
+
     setData((prevData) => {
       return {
         ...prevData,
         isLoading: false,
-      }
-    })
-  }
+      };
+    });
 
-  fetchData();
- }, [url])
+    fetchData();
+  }, [data.isFirstCall, url]);
 
   // async fetchData(url: string) {
   //   try {
@@ -172,30 +198,46 @@ function Home() {
   //   }
   // }
 
-  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    this.setState({
-      isLoading: true,
-    });
-    this.fetchData(`${this.characterByName}${this.state.searching}`);
+    setUrl(`${characterByName}${data.searching}`);
   };
 
-  handleChangeForm = (event: React.ChangeEvent<HTMLInputElement>) => {
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   this.setState({
+  //     isLoading: true,
+  //   });
+  //   this.fetchData(`${this.characterByName}${this.state.searching}`);
+  // };
+
+  const handleChangeForm = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
-    this.setState({
-      searching: value,
+    setData((prevData) => {
+      return {
+        ...prevData,
+        searching: value,
+      };
     });
   };
 
-  handleChange = (id: number) => {
-    this.setState((prevState) => {
-      const updatedCards = prevState.cards.results.map((todo) => {
+  // handleChangeForm = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const { value } = event.target;
+  //   this.setState({
+  //     searching: value,
+  //   });
+  // };
+
+  const handleChange = (id: number) => {
+    setData((prevData) => {
+      const updatedCards = prevData.cards.results.map((todo) => {
         if (todo.id === id) {
           todo.isFavorite = !todo.isFavorite;
         }
         return todo;
       });
       return {
+        ...prevData,
         cards: {
           results: updatedCards,
         },
@@ -203,60 +245,97 @@ function Home() {
     });
   };
 
-  handleClickToggle = (id = 0) => {
-    const cardId = this.state.cards.results.findIndex((item) => item.id === id);
-    if (this.state.isPopup && id) {
+  // handleChange = (id: number) => {
+  //   this.setState((prevState) => {
+  //     const updatedCards = prevState.cards.results.map((todo) => {
+  //       if (todo.id === id) {
+  //         todo.isFavorite = !todo.isFavorite;
+  //       }
+  //       return todo;
+  //     });
+  //     return {
+  //       cards: {
+  //         results: updatedCards,
+  //       },
+  //     };
+  //   });
+  // };
+
+  const handleClickToggle = (id = 0) => {
+    const cardId = data.cards.results.findIndex((item) => item.id === id);
+    if (data.isPopup && id) {
       document.body.classList.remove('stop-scrolling');
-      this.setState({
-        cardModal: this.state.cards.results[cardId],
-        isPopup: false,
+      setData((prevData) => {
+        return {
+          ...prevData,
+          cardModal: data.cards.results[cardId],
+          isPopup: false,
+        };
       });
     } else {
       document.body.classList.add('stop-scrolling');
-      this.setState({
-        cardModal: this.state.cards.results[cardId],
-        isPopup: true,
+      setData((prevData) => {
+        return {
+          ...prevData,
+          cardModal: data.cards.results[cardId],
+          isPopup: true,
+        };
       });
     }
   };
 
-  render() {
-    return (
-      <div className="container">
-        <SearchBar
-          handleChangeForm={this.handleChangeForm}
-          handleSubmit={this.handleSubmit}
-          searching={this.state.searching}
-        />
-        {this.state.errMessage ? (
-          <div className="error-fetch">{this.state.errMessage}</div>
-        ) : (
-          <>
-            {this.state.isLoading
-              ? this.state.isLoading && (
-                  <div className="loader">
-                    <div></div>
-                  </div>
-                )
-              : this.state.cards && (
-                  <>
-                    <CardList
-                      cards={this.state.cards}
-                      handleChange={this.handleChange}
-                      handleClickToggle={this.handleClickToggle}
-                    />
-                    <Popup
-                      card={this.state.cardModal}
-                      active={this.state.isPopup}
-                      handleClickToggle={this.handleClickToggle}
-                    />
-                  </>
-                )}
-          </>
-        )}
-      </div>
-    );
-  }
+  // handleClickToggle = (id = 0) => {
+  //   const cardId = this.state.cards.results.findIndex((item) => item.id === id);
+  //   if (this.state.isPopup && id) {
+  //     document.body.classList.remove('stop-scrolling');
+  //     this.setState({
+  //       cardModal: this.state.cards.results[cardId],
+  //       isPopup: false,
+  //     });
+  //   } else {
+  //     document.body.classList.add('stop-scrolling');
+  //     this.setState({
+  //       cardModal: this.state.cards.results[cardId],
+  //       isPopup: true,
+  //     });
+  //   }
+  // };
+
+  return (
+    <div className="container">
+      <SearchBar
+        handleChangeForm={handleChangeForm}
+        handleSubmit={handleSubmit}
+        searching={data.searching}
+      />
+      {data.errMessage ? (
+        <div className="error-fetch">{data.errMessage}</div>
+      ) : (
+        <>
+          {data.isLoading
+            ? data.isLoading && (
+                <div className="loader">
+                  <div></div>
+                </div>
+              )
+            : data.cards && (
+                <>
+                  <CardList
+                    cards={data.cards}
+                    handleChange={handleChange}
+                    handleClickToggle={handleClickToggle}
+                  />
+                  <Popup
+                    card={data.cardModal}
+                    active={data.isPopup}
+                    handleClickToggle={handleClickToggle}
+                  />
+                </>
+              )}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Home;
