9ec7362c7a375c9de457e247d7cbba6d9eae2732
dab10 <dab10@list.ru>
2022-10-23 03:01:09 +0300

    feat: add form base on useForm

--- react-typescript/src/components/FormFields/InputName/InputName.tsx
+++ react-typescript/src/components/FormFields/InputName/InputName.tsx
@@ -1,39 +1,71 @@
 import React from 'react';
 import MessageError from '../MessageError/MessageError';
 import './InputName.scss';
+import { useForm, UseFormReturn, SubmitHandler } from 'react-hook-form';
 
-interface InputProps
-  extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
-    React.AriaAttributes {}
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
 interface InputNameProps extends InputProps {
   formErrors: {
     name: string;
   };
-  handleChange: () => void;
 }
 
 type Ref = HTMLInputElement;
 
-const InputName = React.forwardRef<Ref, InputNameProps>(
-  ({ handleChange, formErrors, ...props }, ref) => {
-    return (
-      <div>
-        <label>
-          Name:
-          <input
-            className="input-name"
-            type="text"
-            name="name"
-            ref={ref}
-            onChange={handleChange}
-            {...props}
-          />
-        </label>
-        <MessageError messageError={formErrors.name} />
-      </div>
-    );
-  }
-);
+const InputName = React.forwardRef<Ref, InputNameProps>(({ formErrors, ...props }, ref) => {
+  return (
+    <div>
+      <label>
+        Name:
+        <input className="input-name" type="text" name="name" ref={ref} {...props} />
+      </label>
+      <MessageError messageError={formErrors.name} />
+    </div>
+  );
+});
 
 export default InputName;
+
+// import React from 'react';
+// import MessageError from '../MessageError/MessageError';
+// import './InputName.scss';
+
+// interface InputProps
+//   extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
+//     React.AriaAttributes {}
+
+// interface InputNameProps extends InputProps {
+//   formErrors: {
+//     name: string;
+//   };
+//   handleChange: () => void;
+// }
+
+// type Ref = HTMLInputElement;
+
+// const InputName = React.forwardRef<Ref, InputNameProps>(
+//   ({ handleChange, formErrors, ...props }, ref) => {
+//     return (
+//       <div>
+//         <label>
+//           Name:
+//           <input
+//             className="input-name"
+//             type="text"
+//             name="name"
+//             ref={ref}
+//             onChange={handleChange}
+//             {...props}
+//           />
+//         </label>
+//         <MessageError messageError={formErrors.name} />
+//       </div>
+//     );
+//   }
+// );
+
+// export default InputName;
