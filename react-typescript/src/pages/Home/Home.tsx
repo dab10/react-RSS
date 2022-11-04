6984f1b7f39627b52bc99860de9742c4e3d9f9f2
dab10 <dab10@list.ru>
2022-11-04 21:29:12 +0300

    refactor: delete unused vars and comments

--- react-typescript/src/pages/Home/Home.tsx
+++ react-typescript/src/pages/Home/Home.tsx
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './Home.scss';
 import SearchBar from 'components/SearchBar/SearchBar';
 import CardList from 'components/CardList/CardList';
-import { AppContext } from 'context/AppState';
-import { maxLimitPerPage, FilterByStatus, ResultsPerPage, TypeDispatch } from 'utils/const/const';
-import { getPageCount } from 'utils/pagination/getPageCount';
+import { maxLimitPerPage, FilterByStatus, ResultsPerPage } from 'utils/const/const';
 import Pagination from 'components/UI/pagination/Pagination';
 import MySelect from 'components/UI/select/MySelect';
-import { Card } from './Home.types';
 import { useAppDispatch, useAppSelector } from 'store/hooks/redux';
 import { fetchData } from 'store/reducers/ActionCreator';
 import {
@@ -25,84 +22,16 @@ import {
 function Home() {
   const base = 'https://rickandmortyapi.com/api';
   const characterByName = `${base}/character/?name=`;
-  const { state, dispatch } = useContext(AppContext);
-  const dispatch1 = useAppDispatch();
-  const {
-    data,
-    url,
-    query,
-    filterByStatus,
-    limit,
-    totalPages,
-    page,
-    isFirstCall,
-    isError,
-    isLoading,
-  } = useAppSelector((state) => state.homeReducer);
-
-  // const fetchData = async (url: string, limit = maxLimitPerPage, pageNumber = 1) => {
-  //   const arrayFromZeroToMaxLimit = [...Array(maxLimitPerPage).keys()];
-  //   const chunkSize = Math.ceil(maxLimitPerPage / limit);
-  //   let count = 1;
-  //   let sliceLeft;
-  //   let sliceRight;
-
-  //   for (let i = 0; i < maxLimitPerPage; i += limit) {
-  //     const chunk = arrayFromZeroToMaxLimit.slice(i, i + limit);
-  //     const sliceLeftChunk = Math.min(...chunk);
-  //     const sliceRightChunk = Math.max(...chunk);
-
-  //     if (pageNumber % chunkSize === count) {
-  //       sliceLeft = sliceLeftChunk;
-  //       sliceRight = sliceRightChunk + 1;
-  //     }
-  //     count++;
-  //     count === chunkSize ? (count = 0) : count;
-  //   }
-
-  //   try {
-  //     const res = await fetch(url);
-  //     const data = await res.json();
-  //     if (!res.ok) {
-  //       throw Error();
-  //     }
-
-  //     const updatedCards = data.results.slice(sliceLeft, sliceRight).map((item: Card) => {
-  //       item.isFavorite = false;
-  //       return item;
-  //     });
-  //     dispatch({
-  //       type: TypeDispatch.FETCH_SUCCESS,
-  //       payload: {
-  //         homePage: {
-  //           data: updatedCards,
-  //           totalPages: getPageCount(data.info.count, limit),
-  //         },
-  //       },
-  //     });
-  //   } catch {
-  //     dispatch({ type: TypeDispatch.FETCH_ERROR });
-  //   }
-  // };
+  const dispatch = useAppDispatch();
+  const { data, query, filterByStatus, limit, totalPages, page, isFirstCall, isError, isLoading } =
+    useAppSelector((state) => state.homeReducer);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    dispatch1(setUrlAfterSubmit(`${characterByName}${query}`));
-    dispatch1(await fetchData(`${characterByName}${query}&page=1&status=${filterByStatus}`, limit));
+    dispatch(setUrlAfterSubmit(`${characterByName}${query}`));
+    dispatch(await fetchData(`${characterByName}${query}&page=1&status=${filterByStatus}`, limit));
     localStorage.setItem('savedStateSearching', JSON.stringify(query));
   };
-
-  // const handleChangeForm = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const { value } = event.target;
-  //   dispatch({
-  //     type: TypeDispatch.HANDLE_CHANGE_FORM,
-  //     payload: {
-  //       homePage: {
-  //         query: value,
-  //       },
-  //     },
-  //   });
-  // };
 
   const handleChange = (id: number) => {
     const updatedCards = data.map((todo) => {
@@ -111,38 +40,15 @@ function Home() {
       }
       return todo;
     });
-    dispatch1(handleChangeLikes(updatedCards));
+    dispatch(handleChangeLikes(updatedCards));
   };
-
-  // const handleClickToggle = (id = 0) => {
-  //   const cardId = state.homePage.data.findIndex((item) => item.id === id);
-  //   if (id) {
-  //     dispatch({
-  //       type: TypeDispatch.CLOSE_POPUP,
-  //       payload: {
-  //         homePage: {
-  //           dataPopup: state.homePage.data[cardId],
-  //         },
-  //       },
-  //     });
-  //   } else {
-  //     dispatch({
-  //       type: TypeDispatch.OPEN_POPUP,
-  //       payload: {
-  //         homePage: {
-  //           dataPopup: state.homePage.data[cardId],
-  //         },
-  //       },
-  //     });
-  //   }
-  // };
 
   const handleClickToggle = (id = 0) => {
     const cardId = data.findIndex((item) => item.id === id);
     if (id) {
-      dispatch1(closePopup(data[cardId]));
+      dispatch(closePopup(data[cardId]));
     } else {
-      dispatch1(openPopup(data[cardId]));
+      dispatch(openPopup(data[cardId]));
     }
   };
 
@@ -150,8 +56,8 @@ function Home() {
     const chunkSize = Math.ceil(maxLimitPerPage / limit);
     const pageNumberLimit = Math.ceil(pageNumber / chunkSize);
 
-    dispatch1(changePage(pageNumber));
-    dispatch1(
+    dispatch(changePage(pageNumber));
+    dispatch(
       await fetchData(
         `${characterByName}${query}&page=${pageNumberLimit}&status=${filterByStatus}`,
         limit,
@@ -163,32 +69,32 @@ function Home() {
   const handleChangeLimit = async (event: React.ChangeEvent<HTMLSelectElement>) => {
     const { value } = event.target;
 
-    dispatch1(changeLimit(Number(value)));
+    dispatch(changeLimit(Number(value)));
 
     if (!isFirstCall) {
-      dispatch1(
+      dispatch(
         await fetchData(`${characterByName}${query}&page=1&status=${filterByStatus}`, Number(value))
       );
     } else {
-      dispatch1(loadingFalse());
+      dispatch(loadingFalse());
     }
   };
 
   const handleFilterItems = async (event: React.ChangeEvent<HTMLSelectElement>) => {
     const { value } = event.target;
-    dispatch1(filterItems(value));
+    dispatch(filterItems(value));
 
     if (!isFirstCall) {
-      dispatch1(await fetchData(`${characterByName}${query}&page=1&status=${value}`, limit));
+      dispatch(await fetchData(`${characterByName}${query}&page=1&status=${value}`, limit));
     } else {
-      dispatch1(loadingFalse());
+      dispatch(loadingFalse());
     }
   };
 
   return (
     <div className="container">
       <SearchBar
-        handleChangeForm={(event) => dispatch1(handleChangeInput(event.target.value))}
+        handleChangeForm={(event) => dispatch(handleChangeInput(event.target.value))}
         handleSubmit={handleSubmit}
         searching={query}
       />
