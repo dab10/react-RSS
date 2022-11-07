5aa2470e81263b6828c815ee09d9deec1e673197
dab10 <dab10@list.ru>
2022-11-07 23:04:02 +0300

    refactor: delete unused components and vars

--- react-typescript/src/index.tsx
+++ react-typescript/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
-import { AppState } from 'context/AppState';
 import { Provider } from 'react-redux';
 import { setupStore } from 'store/store';
 
@@ -12,9 +11,7 @@ const store = setupStore();
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <AppState>
-        <App />
-      </AppState>
+      <App />
     </Provider>
   </BrowserRouter>
 );
